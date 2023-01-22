91abc0468fae72354e06add5050b9796cefe0f81
Codename14 <46088872+Codename14@users.noreply.github.com>
2023-01-22 19:16:11 +0100

    script fixing

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 let btn = document.querySelector(".cube");
 
-fetch();
+getAdvice();
 
-btn.addEventListener("click", function () {
+btn.addEventListener("click", () => {
+  getAdvice();
+});
+
+function getAdvice() {
   fetch("https://api.adviceslip.com/advice")
     .then((response) => response.json())
     .then((data) => {
       const advice = data.slip.advice;
       document.querySelector(".advice-text").innerHTML = advice;
     });
-});
+}
